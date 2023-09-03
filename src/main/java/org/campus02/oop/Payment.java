11f839298d49df464a245acdb4c1aa8b7d9937d2
Jenny Brunner <00860053@edu.campus02.at>
2023-09-03 13:22:21 +0200

    CreditCardPayment Class finfished, not yet tested

--- src/main/java/org/campus02/oop/Payment.java
+++ src/main/java/org/campus02/oop/Payment.java
@@ -1,2 +1,38 @@
-package org.campus02.oop;public class Payment {
+package org.campus02.oop;
+
+public abstract class Payment {
+    private double amount;
+    private String currency;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Payment(double amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public double exchangeToEUR() {
+        switch (currency) {
+            case "EUR":
+                return amount;
+            case "USD":
+                return amount / 1.1;
+            case "GPB":
+                return amount / 0.85;
+            case "SEK":
+                return amount / 9.5;
+            case "HUF":
+                return amount / 310.0;
+            default:
+                return amount / 2.0;
+        }
+    }
+
+    public abstract double calcTransactionCosts();
 }
